7c0c394135471b43cfd9cabb7d43a1a3ac3c0355
SolidusSnake <SolidusSnake@users.noreply.github.com>
2017-05-11 20:40:31 -0500

    Update Address.java

--- IT210/src/M1/Address.java
+++ IT210/src/M1/Address.java
@@ -9,7 +9,7 @@ public class Address
     public static void main(String[] args)
     {
         System.out.println("Wesley Tomlinson");
-        System.out.println("20565 Prairie Hills Dr");
-        System.out.println("Harrah, OK 73045");
+        System.out.println("STREET ADDR");
+        System.out.println("CITY,ST ZIP");
     }
 }
